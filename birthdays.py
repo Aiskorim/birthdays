476906eb7dba2d64dcecb431faa59caf104296df
Aiskorim <luisayang9@gmail.com>
2020-11-17 12:56:21 +0100

    Added birthday of an important person

--- birthdays.py
+++ birthdays.py
@@ -1,11 +1,12 @@
 z=zhou
-
+#I added my birthday because I'm important too
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
     'Ada Lovelace': '12/10/1815',
     'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
+    'Rowan Atkinson': '01/6/1955'
+    'Luisa Yang': '25/04/1999'}
 
 def print_birthdays():
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
